7b66be389ea373e8e743406019e9c08397dfcd2b
Kostia Savchenko <kostia.xn@gmail.com>
2023-03-04 14:35:01 +0300

    updated function first initializes latest_most_profitable_rate to None, and then updates it whenever a new most profitable rate is found. If a new most profitable rate is found and it's different from the latest one, the function prints a notification with the time when the rate was updated, the buy and sell rates, and the name of the bank.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import datetime
 from openpyxl import Workbook
 import threading
 import os
 import time
 
+from openpyxl.reader.excel import load_workbook
+
+
 # Function to get exchange rates
+last_hour = None
+
 def get_exchange_rates():
     url = 'https://banki24.by/kurs/usd'
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
@@ -22,32 +27,43 @@ def get_exchange_rates():
             if bank_name_element:
                 bank_name = bank_name_element.text.strip()
                 if bank_name:
-                    exchange_buy = cols[1].text.strip()
-                    exchange_sell = cols[2].text.strip()
+                    exchange_buy = cols[1].text.strip().replace(',', '.')
+                    exchange_sell = cols[2].text.strip().replace(',', '.')
                     exchange_rates.append([datetime.now(), bank_name, exchange_buy, exchange_sell])
             else:
                 continue
     return exchange_rates
 
+
+
 # Function to write exchange rates to XLSX file on desktop
 def write_to_xlsx(exchange_rates):
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') # Get desktop path
     file_path = os.path.join(desktop, 'exchange_rates.xlsx')
-    wb = Workbook()
-    ws = wb.active
-    ws.append(['Date', 'Bank Name', 'Buy Rate', 'Sell Rate'])
+
+    if os.path.isfile(file_path):  # if file exists, load existing data
+        wb = load_workbook(file_path)
+        ws = wb.active
+    else:  # otherwise, create new file
+        wb = Workbook()
+        ws = wb.active
+        ws.append(['Date', 'Bank Name', 'Buy Rate', 'Sell Rate'])
+
     for rate in exchange_rates:
         ws.append(rate)
+
     wb.save(file_path)
     print('Exchange rates have been updated and saved to:', file_path)
+
 
 # Function to run the exchange rate fetching and writing process every hour
 def run_process():
     exchange_rates = get_exchange_rates()
     write_to_xlsx(exchange_rates)
     print('Exchange rates have been updated.')
+    latest_most_profitable_rate = None
     while True:
-        time.sleep(300) # Wait 5 minutes
+        time.sleep(3600) # Wait 1 hour
         new_rates = get_exchange_rates()
         if new_rates == exchange_rates:
             print('Exchange rates are up to date.')
@@ -56,6 +72,11 @@ def run_process():
             exchange_rates = new_rates
             write_to_xlsx(exchange_rates)
             print('Exchange rates have been updated.')
+            most_profitable_rate = max(exchange_rates, key=lambda x: float(x[2].replace(',', '.')) - float(x[3].replace(',', '.')))
+            if latest_most_profitable_rate is None or most_profitable_rate[2:] != latest_most_profitable_rate[1:]:
+                latest_most_profitable_rate = (datetime.now(),) + tuple(most_profitable_rate[1:])
+                print(f"Most profitable rate at {latest_most_profitable_rate[0].strftime('%Y-%m-%d %H:%M:%S')}: Buy {latest_most_profitable_rate[2]}, Sell {latest_most_profitable_rate[3]}, Bank {latest_most_profitable_rate[1]}")
+
 
 # Function to start and stop the exchange rate fetching and writing process
 def start_stop_process(start):
